e3596b889461b911b17892c1cd67090d8e556a7a
JUNGSAP <thomas.jung1@gmail.com>
2019-03-29 11:24:50 -0400

    Move credentials to user provided service

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -5,54 +5,72 @@
  * Implementation for CatalogService defined in ./cat-service.cds
  */
 
-const cds = require("@sap/cds");
-
 module.exports = (srv) => {
+
+	const cds = require("@sap/cds");
+	const xsenv = require("@sap/xsenv");
+	const services = xsenv.readCFServices();
+	const s4 = services.s4_sdk_backend || {
+		credentials: {}
+	};
+	const apiKey = s4.credentials.apiKey || "";
+	const s4Url = s4.credentials.url || "";
 
 	const {
 		PO
 	} = cds.entities("opensap.PurchaseOrder");
 
-// If you want to use API Hub instead of local proxy uncomment the withCustomHeaders, fill in your API key and then use the sandbox.api.sap.com URl
 	srv.on("READ", "BusinessPartners", async(req) => {
-		var bp = require("@sap/cloud-sdk-vdm-business-partner-service");
-		let businessPartners = await bp.BusinessPartner.requestBuilder()
-			.getAll()
-			.top(100)
-		//	.withCustomHeaders({apikey: "<your api key here>"})
-			.execute({
-				url: "https://qgyx1mabatjzydhfs4mock-s4mocknode.hanapm.local.com:30033/"
-			//	url: "https://sandbox.api.sap.com/s4hanacloud/"
-			});
-		let data = [];
-		for (let each of businessPartners) {
-			data.push({
-				"BusinessPartner": each.businessPartner,
-				"Category": each.businessPartnerCategory,
-				"FullName": each.businessPartnerFullName,
-				"UUID": each.businessPartnerUuid,
-				"Grouping": each.businessPartnerGrouping
-			});
+		try {
+			var bp = require("@sap/cloud-sdk-vdm-business-partner-service");
+			let businessPartners = await bp.BusinessPartner.requestBuilder()
+				.getAll()
+				.top(100)
+				.withCustomHeaders({
+					apikey: apiKey
+				})
+				.execute({
+					url: s4Url
+				});
+			let data = [];
+			for (let each of businessPartners) {
+				data.push({
+					"BusinessPartner": each.businessPartner,
+					"Category": each.businessPartnerCategory,
+					"FullName": each.businessPartnerFullName,
+					"UUID": each.businessPartnerUuid,
+					"Grouping": each.businessPartnerGrouping
+				});
+			}
+			req.reply(data);
+		} catch (err) {
+			console.error(err.toString());
 		}
-		req.reply(data);
 	});
 
 	srv.after("READ", "POs", async(entities, req) => {
 		console.log(`Data: ${JSON.stringify(req.query)}`);
 		for (let each of entities) {
-			var bp = require("@sap/cloud-sdk-vdm-business-partner-service");
-			let businessPartner = await bp.BusinessPartner.requestBuilder()
-				.getByKey(each.PARTNERS_BusinessPartner)
-				.execute({
-					url: "https://qgyx1mabatjzydhfs4mock-s4mocknode.hanapm.local.com:30033/"
-				});
-			each.PARTNERS= {
-				"BusinessPartner": businessPartner.businessPartner,
-				"Category": businessPartner.businessPartnerCategory,
-				"FullName": businessPartner.businessPartnerFullName,
-				"UUID": businessPartner.businessPartnerUuid,
-				"Grouping": businessPartner.businessPartnerGrouping
-			}; 
+			const bp = require("@sap/cloud-sdk-vdm-business-partner-service");
+			try {
+				let businessPartner = await bp.BusinessPartner.requestBuilder()
+					.getByKey(each.PARTNERS_BusinessPartner)
+					.withCustomHeaders({
+						apikey: apiKey
+					})
+					.execute({
+						url: s4Url
+					});
+				each.PARTNERS = {
+					"BusinessPartner": businessPartner.businessPartner,
+					"Category": businessPartner.businessPartnerCategory,
+					"FullName": businessPartner.businessPartnerFullName,
+					"UUID": businessPartner.businessPartnerUuid,
+					"Grouping": businessPartner.businessPartnerGrouping
+				};
+			} catch (err) {
+				console.error(err.toString());
+			}
 		}
 	});
 };
